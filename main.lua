0d5a6d0d4e087aab21693bdafcef4cdf5b7f54be
FrootLoopsBowl <froott@protonmail.com>
2025-10-20 21:12:47 -0400

    Remove shit

--- main.lua
+++ main.lua
@@ -1,7 +1,3 @@
-local colorR = math.random(255)
-local colorG = math.random(255)
-local colorB = math.random(255)
-
 local xRandom = math.random(600)
 local yRandom = math.random(600)
 
@@ -10,7 +6,6 @@ function love.load()
 end
 
 function love.draw()
-  love.graphics.setColor(colorR, colorG, colorB)
   love.graphics.print("Hello World", 100, 100)
   love.graphics.print("HELLOEEEE", xRandom, yRandom)
 end
@@ -18,9 +13,6 @@ end
 
 function love.keypressed(key)
   if key == "b" then
-    colorR = math.random(255)
-    colorG = math.random(255)
-    colorB = math.random(255)
     xRandom = math.random(600)
     yRandom = math.random(600)
   end
